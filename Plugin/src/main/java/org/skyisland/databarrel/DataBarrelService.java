d150e2db6e49faded2f75dde5072f812355a62e8
VaccinoPower <durdom2228@gmail.com>
2025-07-13 00:14:02 +0700

    #3 Add S3 support to DataBarrelService

--- Plugin/src/main/java/org/skyisland/databarrel/DataBarrelService.java
+++ Plugin/src/main/java/org/skyisland/databarrel/DataBarrelService.java
@@ -1,10 +1,12 @@
 package org.skyisland.databarrel;
 
 import com.zaxxer.hikari.HikariDataSource;
+import software.amazon.awssdk.services.s3.S3Client;
 
 public interface DataBarrelService {
 
     HikariDataSource getHikariDataSource(String name);
+    S3Client getS3Client(String name);
 
     static DataBarrelService getInstance() {
         return DataBarrelBootstrap.bedrockDataService;
